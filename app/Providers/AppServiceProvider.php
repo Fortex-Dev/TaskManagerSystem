284f7d0bc0a0a409802dd9abbb7b12db773de57c
FortexGit <hesabate.ll3amel.fealdev.fortex@gmail.com>
2026-08-30 23:37:38 +0300

    Fix AppServiceProvider HTTPS import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,6 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    use Illuminate\Support\Facades\URL;
-
 public function boot(): void
 {
     if (app()->environment('production')) {
